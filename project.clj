0677210ae3b08355252ef73a3ff19b572cf99c08
Gareth M <gareth@local>
2018-01-14 19:56:36 +0000

    Make cgrand/moustache a git dependency instead of a clojars dependency. Without this, installation of dependencies fails with TLS error.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject enlive-tutorial "0.1.0"
   :description "Enlive Tutorial"
+  :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [enlive "1.1.1"]
-                 [ring "1.2.0"]
-                 [net.cgrand/moustache "1.1.0"]])
+                 [ring "1.2.0"]]
+  :git-dependencies [[github.com:cgrand/moustache.git]]
+  )
